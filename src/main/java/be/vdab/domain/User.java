79c548e7be38ab8fa1ff8f7bc3743a7653e08953
LarsdeGraaff <lars.dg@gmail.com>
2015-07-01 16:34:07 +0200

    added primeFaces

--- src/main/java/be/vdab/domain/User.java
+++ src/main/java/be/vdab/domain/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Past;
 import java.util.Date;
 
 @Entity
@@ -16,6 +17,7 @@ public class User {
 
     private String lastName;
 
+    @Past
     private Date birthDate;
     private Gender gender;
 
